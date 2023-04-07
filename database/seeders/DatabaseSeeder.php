2bed13345afd4a24c181019f2da626b220ae333f
Kevin DeBrecht <99675851+kd-at-mi@users.noreply.github.com>
2023-04-07 17:50:19 -0500

    feat: remove facade hash

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\User;
+use build\UserSeeder;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
